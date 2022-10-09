d518e15b8eeda6d93ab11a0f111ac7820f8fc159
vargascm <vargassalazar.cm@gmail.com>
2022-10-09 08:29:23 -0700

    script 6

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - Prints "Size in nytes of a char, int, long, long long, float"
+ * Return: 0
+ */
+int main(void)
+{
+	printf("Size of a char %c :\n", sizeof(char);
+	printf("Size of an int %d :\n", sizeof(int);
+	printf("Size of a long %ld :\n", sizeof(long int);
+	printf("Size of a long long %lld :\n", sizeof(long long int);
+	printf("Size of a float %f :\n", sizeof(float);
+
+	return (0);
+}
