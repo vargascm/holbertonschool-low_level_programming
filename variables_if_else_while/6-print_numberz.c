4712c2721f8797698be17ec595e898e5cda72ad7
vargascm <vargassalazar.cm@gmail.com>
2022-10-10 21:40:28 -0700

    .c 06-01

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Main function numberz
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	int l;
+
+	for (l = 0; l < 10; ++l)
+	{
+		putchar(l + '0');
+	}
+
+	putchar('\n');
+
+	return (0);
+}
