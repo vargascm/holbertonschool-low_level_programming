5f4e2d06156ee47b8fb372c94922c7ae3f56a9c3
vargascm <vargassalazar.cm@gmail.com>
2022-10-10 21:26:16 -0700

    .c 04-01

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - Main function
+ *
+ * Return: return 0
+ */
+
+int main(void)
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; ++c)
+	{
+		if (c == 'e' || c == 'q')
+			continue;
+
+		putchar(c);
+	}
+
+
+
+	putchar('\n');
+
+	return (0);
+}
