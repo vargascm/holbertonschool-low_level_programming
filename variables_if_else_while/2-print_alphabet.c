6a314566a03655671bdc41986018743be9fdb20f
vargascm <vargassalazar.cm@gmail.com>
2022-10-10 20:27:56 -0700

    .c 02

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - print of the alphabet lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= 'z' ; c++)
+	{
+		putchar(c);
+	}
+
+	return (0);
+}
