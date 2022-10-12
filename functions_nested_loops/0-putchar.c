55f5ccc4422d081da5545b1364b314391257a702
vargascm <vargassalazar.cm@gmail.com>
2022-10-11 18:35:03 -0700

    .c 01-02

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,9 +1,20 @@
-#include <stdio.h>
+/**
+ * main - prints "_putchar"
+ * Return: Always 0
+ */
 
-int main()
+#include "main.h"
+
+int main(void)
 {
-	printf("_putchar");
-
-	return 0;
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
