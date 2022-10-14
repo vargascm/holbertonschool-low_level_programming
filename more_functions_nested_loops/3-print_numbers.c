8f25e19d8a3fa9b890186381f51d12b6ee66d81f
vargascm <vargassalazar.cm@gmail.com>
2022-10-14 12:01:04 -0700

    more loops 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_numbers() : prints int from output
+ *
+ *Return: void
+ *
+ */
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		_putchar(n + '0');
+	}
+	_putchar('\n');
+}
