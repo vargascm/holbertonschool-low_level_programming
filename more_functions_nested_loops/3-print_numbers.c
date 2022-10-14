72e55b8d6edb40664270e3fa9b3f0a6ecd9c6ed4
vargascm <vargassalazar.cm@gmail.com>
2022-10-14 12:04:11 -0700

    more loops 3-1

--- more_functions_nested_loops/3-print_numbers.c
+++ more_functions_nested_loops/3-print_numbers.c
@@ -1,10 +1,8 @@
 #include "main.h"
 
 /**
- *print_numbers() : prints int from output
- *
- *Return: void
- *
+ * print_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * Return: Always 0
  */
 
 void print_numbers(void)
