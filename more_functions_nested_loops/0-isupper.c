c03800bc56074c05667b1c2d38498e0ad7d294aa
vargascm <vargassalazar.cm@gmail.com>
2022-10-14 07:50:32 -0700

    001

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * _isupper - checks for uppercase character.
@@ -6,10 +6,14 @@
  * Return: 1 if c is uppercase, 0 otherwise
  */
 
-int _isupper(int c);
+int _isupper(int c)
 {
 	if (c >= 65 && c <= 90)
+	{
 		return (1);
+	}
 	else
+	{
 		return (0);
+	}
 }
