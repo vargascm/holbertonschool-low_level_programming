757a932aac45ea9f0612666bebd8b517b369c568
vargascm <vargassalazar.cm@gmail.com>
2022-10-19 07:39:28 -0700

    pointers reverse

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -1,46 +1,15 @@
 #include "main.h"
-
 /**
- * _strlen - returns the length of a string.
- * @s: Pointer to string to measure
- * Return: Lenght of the string
+ * rev_string - Write a function that reverses a string
+ * @s: data
  */
-
-int _strlen(char *s)
+void print_rev(char *s)
 {
-	int i;
-	int len;
+	int i, c = 0;
 
-	i = 0;
-	len = 0;
-	while (*(s + i) != '\0')
-	{
-		len = len + 1;
-		i++;
-	}
-	return (len);
-}
-
-/**
- * rev_string - reverses a string.
- * @s: Pointer to string to reverse
- * Return: Always 0
- */
-
-void rev_string(char *s)
-{
-	int i;
-	int j;
-	char c;
-
-	i = _strlen(s) - 1;
-	j = 0;
-	while (i >= j)
-	{
-		c = *(s + j);
-		*(s + j) = *(s + i);
-		*(s + i) = c;
-		i = i - 1;
-		j++;
-	}
+	for (i = 0; s[i] != '\0'; i++)
+		c++;
+	for (i = c - 1; i >= 0; i--)
+		_putchar(s[i]);
+	_putchar('\n');
 }
