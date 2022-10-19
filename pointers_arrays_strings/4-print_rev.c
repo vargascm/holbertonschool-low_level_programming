59e0fec0825545a79aec9f495e9ee6bf344eea76
vargascm <vargassalazar.cm@gmail.com>
2022-10-18 21:59:05 -0700

    pointer reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_rev - reverses a string.
+ * @s: Pointer to string to reverse
+ * Return: Always 0
+ */
+
+void print_rev(char *s)
+{
+	int i, c = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+		c++;
+	for (i = c - 1; i >= 0; i--)
+		_putchar(s[i]);
+}
