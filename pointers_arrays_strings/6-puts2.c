c9f974354f5ff156fe9d6cc7584d7b41c109c9d2
vargascm <vargassalazar.cm@gmail.com>
2022-10-19 20:50:21 -0700

    pointers print only pair numbers

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - prints every other character of a string, starting
+ * with the first character, followed by a new line.
+ * @str: Pointer to string to print
+ * Return: Always 0
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i]; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
