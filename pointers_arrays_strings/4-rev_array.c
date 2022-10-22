0749f20ddb8fec4cb0fe88997df4a83f3c15da2e
vargascm <vargassalazar.cm@gmail.com>
2022-10-21 19:47:26 -0700

    pointer rev array2

--- pointers_arrays_strings/4-rev_array.c
+++ pointers_arrays_strings/4-rev_array.c
@@ -12,8 +12,8 @@ void reverse_array(int *a, int n)
 
 	for (i = 0; i < (n / 2); i++)
 	{
-		tmp = a[i];
-		a[i] = a[n - i - 1];
-		a[n - i - 1];
+		tmp = *(a + i);
+		*(a + i) = *(a + (n - 1));
+		*(a + (n - 1)) = tmp;
 	}
 }
